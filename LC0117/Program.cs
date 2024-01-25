ea046a53677371c395e21a77308eab7e798d8fcf
Alexandra Camarena <alexandracamarena@ou.edu>
2024-01-24 19:35:32 -0600

    Add project files.

---
+++ LC0117/Program.cs
@@ -0,0 +1,42 @@
+﻿
+Console.WriteLine("Welcome to MIS 3013 001! Spring 2024");
+// comment
+// line of code, statement
+// Python Java C++ R
+// 1940s
+// Programming to use resources of the computer
+// CPU:
+// memory RAM data
+// B: >
+// C => C++ => C++++ # C#
+// C# MS 2000 Java
+
+// .NET 8.0 DOT NET
+// Cars: Ford, Toyota, Honda
+// Toyota: Camry, Civic
+// Camry 2024
+// Camry 2023
+// Camry
+// Console.WriteLine("Welcome to MIS 3013 001! Spring 2024");
+// red ~~~ means syntax error
+
+// comment single line comment
+// another comment
+// another comment
+
+/*
+comment
+comment
+*/
+
+//my code;
+// define a variable
+// 1. letters 0-9 _: only these three
+// 2. the first character can only be letter or _.
+// 2. the number could not be the first chracter
+
+
+// define variables
+// datatype variablename;
+int age1;
+double weight1;
